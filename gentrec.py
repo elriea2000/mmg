44e2ccce42d4582a45b20e02a4cafcdd32f7cafd
elriea2000 <elriea2000@yahoo.co.jp>
2026-06-19 22:18:02 +0900

    apply complessing

--- gentrec.py
+++ gentrec.py
@@ -2,11 +2,13 @@
 
 import glob
 import io
+import zlib
+import base64
 
 fns=glob.glob("./outputs/output_*.json")
 
 sout=io.StringIO()
-sout.write("const trec=\n")
+# sout.write("const trec=\n")
 sout.write("[\n")
 for i,fn in enumerate(fns):
     with open(fn,"r",encoding="utf8") as fp:
@@ -16,5 +18,14 @@ for i,fn in enumerate(fns):
     sout.write("\n")
 sout.write("]\n")
 
-with open("./docs/mmgp_viewer_src.js","w",encoding="utf8") as fp:
-    fp.write(sout.getvalue())
+#with open("./docs/mmgp_viewer_src.js","w",encoding="utf8") as fp:
+#    fp.write(sout.getvalue())
+
+zsrc=zlib.compress(sout.getvalue().encode("utf8"))
+b64zsrcs=base64.b64encode(zsrc).decode("ascii")
+
+with open("./test.bin", "wb") as fp:
+    fp.write(zsrc)
+
+with open("./docs/mmgp_viewer_src.js", "w", encoding="utf8") as fp:
+    fp.write("const _tsrc=\""+b64zsrcs+"\";")
