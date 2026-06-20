1ae7973faee9b8c98ea1d460a03bde31256f9096
elriea2000 <elriea2000@yahoo.co.jp>
2026-06-20 12:05:04 +0900

    藤原竜也対策

--- m2ngp.py
+++ m2ngp.py
@@ -15,11 +15,11 @@ emojis_mbk=[e for e in emojis if e[1]!=""]
 emojis_mbk_inv={e[1]:e[0] for e in emojis_mbk}
 
 # ターゲットファイル
-# fn="./snapshots/snapshot_202606192220.html"
-if len(sys.argv)>1:
-    fn=sys.argv[1]
-else:
-    fn=glob.glob("./snapshots/snapshot_*.html")[-1]
+fn="./snapshots/snapshot_202606201118.html"
+# if len(sys.argv)>1:
+#     fn=sys.argv[1]
+# else:
+#     fn=glob.glob("./snapshots/snapshot_*.html")[-1]
 tstamp=re.split("(\.|_)",os.path.split(fn)[-1])[2]
 
 # 無視するレス番一覧（削除されたものとか）
@@ -36,22 +36,38 @@ data={"tstamp":tstamp,"data":[]}
 for ic,card in enumerate(cards):
     # 無視
     if ic in ignore_id: continue
-    # リンクのインラインを削除
-    for b in card.select("div[class=\"my-1 h-32 w-full max-w-xl rounded-md border bg-muted/80\"]"):
-        b.replace_with("")
+
+    # # message-contentを探す
+
+    card_mc = card.select_one("div.message-content")
+    card_bc = card_mc.parent.next_sibling
+
+    # # リンクのインラインを削除
+    # for b in card.select("div[class=\"my-1 h-32 w-full max-w-xl rounded-md border bg-muted/80\"]"):
+    #     b.replace_with("")
+
     # 名前など抽出
-    cbody=card.findChildren("div", recursive=False)[1].findChild("div").findChildren("div", recursive=False)[0]
+    cbody=card_mc
     for b in cbody.find_all("br"):
         b.replace_with("\n")
-    cbodys=[re.sub("(^\s+|\s+$)","",r) for r in cbody.get_text().split("\n") if r!=""]
-    cbtitle=re.sub("(^・|)","",cbodys[0])
-    cbinfo="\n".join(cbodys[1:])
+    # 藤原竜也対策
+    if ic==120:
+        cbtitle="藤原竜也2"
+        cbinfo=cbody.get_text()
+    elif ic==121:
+        cbtitle="藤原竜也3"
+        cbinfo=cbody.get_text()
+    else:
+        cbodys=[re.sub("(^\s+|\s+$)","",r) for r in cbody.get_text().split("\n") if r!=""]
+        cbtitle=re.sub("(^・|)","",cbodys[0])
+        cbinfo="\n".join(cbodys[1:])
+    
     # 絵文字抽出
-    btns = card.select("div:nth-of-type(2)>div:nth-of-type(2)>button")
+    btns = card_bc.findChildren("button",recursive=False)
     dbtns=[]
     for btn in btns:
-        spans = btn.select("span")
-        bsrc = spans[0].select_one("img").attrs["src"]
+        spans = btn.findChildren("span",recursive=False)
+        bsrc = spans[0].findChild("img").attrs["src"]
         if bsrc[:26]=="https://storage.mebuki.moe":
             balt=emojis_mbk_inv[bsrc]
         else:
